630e6376b753610bb520881ff93d6bcb45e0ce19
Jessica Choe <jessj.choe@gmail.com>
2021-11-22 15:38:45 +0900

    set up api services for posts

---
+++ client/src/services/post.js
@@ -0,0 +1,27 @@
+import api from './api-config';
+
+export const createPost = async (user_id, data) => {
+  const resp = await api.post(`/users/${user_id}/posts`, { topic: data })
+  console.log(resp)
+  return resp.data
+}
+
+export const readAllPosts = async (user_id) => {
+  const resp = await api.get(`/users/${user_id}/posts`)
+  return resp.data
+}
+
+export const readOnePost = async (id) => {
+  const resp = await api.get(`/posts/${id}`);
+  return resp.data
+}
+
+export const updatePost = async (user_id, id, data) => {
+  const resp = await api.put(`users/${user_id}/posts/${id}`, { topic: data })
+  return resp.data
+}
+
+export const destroyPost = async (user_id, id) => {
+  const resp = await api.delete(`users/${user_id}/posts/${id}`)
+  return resp.data
+}
